3dd7ed167dd92f6ce446b73ddd16bf1a3def9090
Sneha Agarwal <73896596+SnehaAgarwal361@users.noreply.github.com>
2021-08-20 17:58:49 +0530

    Create index.js

---
+++ Milestone#3/index.js
@@ -0,0 +1,34 @@
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #b4f2e1;
+  box-sizing: border-box;
+  padding: 0%;
+  font-family: "Josefin Sans", sans-serif;
+}
+div{
+  width:100%;
+  height:100vh;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -30px;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+h1{
+  padding: 20px 20px;
+  background-color:azure;
+  color:#fa9191;
+  border-radius: 20px;
+}
+span{
+  color:#eb6383;
+}
